6ae60c18e32bf2608a99e5018af3d5d7295b1b2e
eleyne <eleynekrd@gmail.com>
2017-12-17 23:05:37 -0600

    Subiendo Archivos

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+/*nav*/
+$(".button-collapse").sideNav();
+/*MODAL*/
+$(document).ready(function(){
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+  });
+/*SELECT*/
+$(document).ready(function() {
+    $('select').material_select();
+  });
